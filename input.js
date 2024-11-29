8c853632b98c6592a02fd41a8f61193697a496b6
Bogdan Alexander <heugasse@gmail.com>
2024-11-29 04:39:40 +0100

    remove input

--- input.js
+++
@@ -1,9 +0,0 @@
-var inputevent = require('input-event');
-
-var k = new inputevent('event0'); // 'event0' is the file corresponding to my keyboard in /dev/input/
-k.on('keyup', console.log);
-k.on('keydown', console.log);
-k.on('keypress', console.log);
-k.on('rel', console.log);
-k.on('abs', console.log);
-k.on('syn', console.log);
